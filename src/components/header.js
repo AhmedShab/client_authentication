36911dc66c63b355cb1e5ca6eeb85ba5e3805ac5
AhmedShab <ahmed.vuw@gmail.com>
2017-02-13 11:46:59 +1300

    added header with a sign in text

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-ligh">
+        <ul className="navbar navbar-nav">
+          <li className="nav-item">Sign in</li>
+        </ul>
+      </nav>
+    );
+  }
+}
